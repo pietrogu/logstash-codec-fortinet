f37832a0055529be05a9ec33badfa7b905ad7927
pietrogu <52070642+pietrogu@users.noreply.github.com>
2019-07-12 04:41:57 -0700

    Add files via upload

--- lib/logstash/codecs/fortinet.rb
+++ lib/logstash/codecs/fortinet.rb
@@ -4,17 +4,17 @@ require "logstash/codecs/base"
 
 class LogStash::Codecs::Fortinet < LogStash::Codecs::Base
   config_name "fortinet"
-
-  # Cache of a gsub pattern that matches a backslash-escaped backslash or backslash-escaped equals, _capturing_ the escaped character
-  EXTENSION_VALUE_ESCAPE_CAPTURE = /\\([\\=])/
-
-  # Serve per individuare le key nel messaggio
-  EXTENSION_KEY_PATTERN = /(?:\w+(?:\.[^\s]\w+[^\|\s\.\=\\]+)?(?==))/
-  # Serve per individuare i value della sezione Extension
-  EXTENSION_VALUE_PATTERN = /(?:\S|\s++(?!#{EXTENSION_KEY_PATTERN}=))*/
-  # Metto insieme per ottenere l'espressione che mi permette di individuare le coppie key/value
-  EXTENSION_KEY_VALUE_SCANNER = /(#{EXTENSION_KEY_PATTERN})=(#{EXTENSION_VALUE_PATTERN})\s*/
-
+  
+  # Regexp per individuare le coppie key-value nel messaggio
+  KEY_PATTERN = /(?:\w+(?:\.[^\s]\w+[^\|\s\.\=\\]+)?(?==))/
+  VALUE_PATTERN = /(?:\S|\s++(?!#{KEY_PATTERN}=))*/
+  KEY_VALUE_SCANNER = /(#{KEY_PATTERN})=(#{VALUE_PATTERN})\s*/
+  
+  # Regexp per trovare nelle key sintassi simile a quella di un array
+  KEY_ARRAY_CAPTURE = /^([^\[\]]+)((?:\[[0-9]+\])+)$/
+  # Regexp per trovare nei value degli escape character (backslash e uguale) 
+  VALUE_ESCAPE_CAPTURE = /\\([\\=])/
+  
   public
   def initialize(params={})
     super(params)
@@ -23,11 +23,10 @@ class LogStash::Codecs::Fortinet < LogStash::Codecs::Base
     @utf8_charset.logger = self.logger
   end
    
-  # In questa sezione effettuiamo il parsing
+  # Con il seguent metodo effettuiamo il parsing
   def decode(data, &block)
     # Creiamo l'evento
     event = LogStash::Event.new
-
     # Usiamo per il log la codifica UTF-8
     @utf8_charset.convert(data)
     # Se l'encoding non ha avuto successo non andiamo avanti nel parsing, perchè nascerebbero errori
@@ -41,28 +40,30 @@ class LogStash::Codecs::Fortinet < LogStash::Codecs::Base
     # Il log da parsare viene inserito in una variabile dal nome message
     message = data
 
-    # Se la variabile messaggio è impostato e contiene degli uguali
+    # Se la variabile messagge contiene degli uguali procediamo con il parsing
     if message && message.include?('=')
-      # Leviamo dal messaggio eventuali caratteri di spazio alla fine e all'inizio
+      # strip permette di eliminare eventuali caratteri di spazio alla fine e all'inizio
       message = message.strip
-      # Scopo di questo ciclo è ricavare le diverse coppie key/value del messaggio
-      message.scan(EXTENSION_KEY_VALUE_SCANNER) do |extension_field_key, raw_extension_field_value|
+      # Ricaviamo le diverse coppie key/value del messaggio
+      message.scan(KEY_VALUE_SCANNER) do |field_key, raw_field_value|
         # Con il seguente comando evitiamo che campi con sintassi simile a quella di un array possano creare errori
-        extension_field_key = extension_field_key.sub(EXTENSION_KEY_ARRAY_CAPTURE, '[\1]\2') if extension_field_key.end_with?(']')
-        # Controlliamo la presenze di sequenze di escape e di simboli ", poi rimuoviamo per evitare problemi in output
-	extension_field_value = raw_extension_field_value.gsub(EXTENSION_VALUE_ESCAPE_CAPTURE, '\1').gsub(/["]/,'')
-	
-	# A questo punto nell'evento settiamo la coppia key-value trovata
-      event.set(extension_field_key, extension_field_value)
+        field_key = field_key.sub(KEY_ARRAY_CAPTURE, '[\1]\2') if field_key.end_with?(']')
+        # Gestiamo la presenza di escape sequence e dei doppi apici
+        field_value = raw_field_value.gsub(VALUE_ESCAPE_CAPTURE, '\1').gsub(/["]/,'')
+        # A questo punto nell'evento settiamo la coppia key-value trovata
+        event.set(field_key, field_value)
       end
     end
+
+    # Aggiungiamo il log non parsato
+    event.set("RAW_MESSAGE", data.gsub(/["]/,''))
 
     # Portiamo in uscita l'evento
     yield event
 
     # In caso di errore viene mostrato il seguente messaggio
-  rescue => e
-    @logger.error("Failed to decode Fortinet payload. Generating failure event with payload in message field.", :error => e.message, :backtrace => e.backtrace, :data => data)
-    yield LogStash::Event.new("message" => data, "tags" => ["_Fortinetparsefailure"])
-  end
+    rescue => e
+      @logger.error("Failed to decode Fortinet payload. Generating failure event with payload in message field.", :error => e.message, :backtrace => e.backtrace, :data => data)
+      yield LogStash::Event.new("message" => data, "tags" => ["_Fortinetparsefailure"])
+    end
 end
